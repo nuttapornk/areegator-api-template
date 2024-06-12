97ff714746f12b91af5767c9f40acba22c3dd205
nuttaporn kunsin <to_andata@hotmail.com>
2024-06-13 00:18:08 +0700

    update lib and startupHealthCheck

---
+++ src/Infrastructure/HealthChecks/StartupHealthCheck.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Infrastructure.HealthChecks;
+
+public class StartupHealthCheck : IHealthCheck
+{
+    private volatile bool _isReady;
+
+    public bool StartupCompleted
+    {
+        get => _isReady;
+        set => _isReady = value;
+    }
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(HealthCheckResult.Healthy("The startup task has completed."));
+    }
+}
--- src/_NTLCOMPONENT_/ConfigureService.cs
+++ src/_NTLCOMPONENT_/ConfigureService.cs
@@ -1,6 +1,10 @@
 using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Filters;
 using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Helpers.NSwag;
+using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Infrastructure.HealthChecks;
+using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Infrastructure.MessageBrokers;
+using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Infrastructure.Persistence;
 using _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_.Middlewares;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Newtonsoft.Json.Serialization;
 
 namespace _NTLPLATFORM_._NTLDOMAIN_._NTLCOMPONENT_;
@@ -12,7 +16,23 @@ public static class ConfigureService
     {
         services.AddHttpContextAccessor();
 
-        services.AddHealthChecks();
+        //Add Cors
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CORS", corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+        });
+
+        services.AddHealthChecks()
+            .AddCheck<StartupHealthCheck>("Startup", tags: new[] { "startup" })
+            .AddDbContextCheck<AppDbContext>(tags: new[] { "ready", "live" })            
+            .AddKafkaCheck(
+                bootstrapServers: MessageBrokersCollectionExtensions.GetKafkaOption(configuration).BootstrapServers,
+                topic: "healthcheck",
+                name: "Message Broker (Kafka)",
+                failureStatus: HealthStatus.Degraded, tags: new[] { "ready", "live" });
 
         services.AddOpenApiDocument(config =>
         {
@@ -27,22 +47,10 @@ public static class ConfigureService
         {
             options.Filters.Add<AddHttpContextItemsActionFilter>();
             options.Filters.Add<ApiExceptionFilterAttribute>();
-        });
-
-        //Add Cors
-        services.AddCors(options =>
+        }).AddNewtonsoftJson(options =>
         {
-            options.AddPolicy("CORS", corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-            );
-        });
-
-        services.AddControllers()
-            .AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-            });
+            options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+        });            
 
         //Middleware     
         services.AddTransient<LoggingMiddleware>();
